ee7933531c1d054ca9a0695a9f6e342d2a95bb4f
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-11-19 18:28:23 +0530

    fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 4000;
 // Define the target URLs for your microservices
 const service1Url = 'http://localhost:4001';
 const inventoryService2Url = 'http://localhost:4002';
+// const LiveNtifacition = 'http://localhost:4004';
 
 // Proxy requests for Service 1
 app.use('/user', createProxyMiddleware({
