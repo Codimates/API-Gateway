296182bc8b72ece5254cc20b72ec203f5f9d4a83
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-12-05 12:00:41 +0530

    test d1

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const router = express.Router();
+const cors = require('cors');
+
+router.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000' || 'http://localhost:3001' || 'http://141.144.204.231' || /^http:\/\/.*:3000$/,
+  })
+)
 
 const app = express();
 const port = 4000;
 
 // Define the target URLs for your microservices
-const service1Url = 'http://localhost:4001';
-const inventoryService2Url = 'http://localhost:4002';
+const service1Url = 'http://141.144.204.231:4001';
+const inventoryService2Url = 'http://141.144.204.231:4002';
 
 // Proxy requests for Service 1
 app.use('/user', createProxyMiddleware({
@@ -28,5 +37,5 @@ app.use('/inventory', createProxyMiddleware({
 
 // Start the API Gateway server
 app.listen(port, () => {
-  console.log(`API Gateway is running on http://localhost:${port}`);
+  console.log(`API Gateway is running on port ${port}`);
 });
